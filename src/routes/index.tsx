c15e5e4c8f96e9a9281453c2a94cb09a6b7e3f60
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-20 16:11:53 +0200

    Change index component name to HomeComponent and implement error and loading views

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import useGetPodcasts from "../utils/useGetPodcasts";
 
-export const Route = createFileRoute('/')({
-  component: HomeComponent,
-})
+export const Route = createFileRoute("/")({
+  component: Home,
+});
 
-function HomeComponent() {
+function Home() {
+  const { data, isLoading, error } = useGetPodcasts();
+
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="container mx-auto">
+      {isLoading && (
+        <main className="flex justify-center items-center h-screen">
+          <h1 className="text-5xl">Loading podcasts...</h1>
+        </main>
+      )}
+
+      {!isLoading && error && (
+        <main className="flex justify-center items-center h-screen">
+          <h1>We are sorry! Something went wrong</h1>
+        </main>
+      )}
+
+      {!isLoading && data && (
+        <h1>Loaded</h1>
+      )}
     </div>
-  )
+  );
 }
