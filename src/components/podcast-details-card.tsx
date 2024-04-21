545150a03b986bf061ee25f69213729e7b40dbba
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-21 21:02:46 +0200

    Implement podcast-details-card

---
+++ src/components/podcast-details-card.tsx
@@ -0,0 +1,36 @@
+import Card from "./card";
+
+interface Props {
+  lowResCover: string;
+  title: string;
+  artist: string;
+  description: string;
+}
+
+const PodcastDetailsCard = ({ lowResCover, title, artist, description }: Props) => {
+  return (
+    <Card className="w-full max-w-xs">
+      <div className="divide-y">
+        <section className="p-4">
+          <img
+            className="mx-auto w-48 h-w-48 rounded-md transition-all"
+            src={lowResCover}
+            alt={`Cover image for the podcast ${title}.`}
+          />
+        </section>
+
+        <section className="p-4">
+          <h2 className="text-lg font-bold">{title}</h2>
+          <p>by {artist}</p>
+        </section>
+
+        <section className="p-4">
+          <p className="font-bold">Description</p>
+          <p>{description}</p>
+        </section>
+      </div>
+    </Card>
+  );
+};
+
+export default PodcastDetailsCard;
