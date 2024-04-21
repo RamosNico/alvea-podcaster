35b10784e1f4329ccc2ca32504c778dfe0b0fcc6
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-21 22:04:25 +0200

    Improve podcastId component's layout and add EpisodesTable

--- src/routes/podcast/$podcastId.tsx
+++ src/routes/podcast/$podcastId.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import fetchPodcastDetails from "../../utils/fetch-podcast-details";
 import PodcastDetailsCard from "../../components/podcast-details-card";
 import Card from "../../components/card";
+import EpisodesTable from "../../components/episodes-table";
 
 export const Route = createFileRoute("/podcast/$podcastId")({
   loader: async ({ params }) => {
@@ -23,17 +24,23 @@ function PodcastPage() {
   const data = Route.useLoaderData();
 
   return (
-    <main className="pt-8 container mx-auto grid grid-cols-2 gap-x-12">
-      <PodcastDetailsCard
-        lowResCover={data.lowResCover}
-        title={data.title}
-        artist={data.artist}
-        description={data.description}
-      />
+    <main className="pt-8 px-4 container mx-auto max-lg:space-y-8 lg:flex gap-x-4 xl:gap-x-12">
+      <section className="flex justify-center lg:block w-full">
+        <PodcastDetailsCard
+          lowResCover={data.lowResCover}
+          title={data.title}
+          artist={data.artist}
+          description={data.description}
+        />
+      </section>
 
       <section className="space-y-4">
         <Card className="w-full p-4">
           <p className="text-2xl font-bold">Episodes: {data.totalEpisodes}</p>
+        </Card>
+
+        <Card className="w-full p-4">
+          <EpisodesTable episodes={data.episodes} />
         </Card>
       </section>
     </main>
