be87ac4906d441e791eac4f8a7a6a3b60843da8d
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-22 01:32:13 +0200

    Fix podcast-details-card for the episodeId page by passing the feedUrl prop

--- src/routes/podcast/$podcastId.episode.$episodeId.tsx
+++ src/routes/podcast/$podcastId.episode.$episodeId.tsx
@@ -26,10 +26,10 @@ function EpisodePage() {
     <main className="pt-8 px-4 container mx-auto max-lg:space-y-8 lg:flex gap-x-4 xl:gap-x-12">
       <section className="flex justify-center lg:block w-full">
         <PodcastDetailsCard
+          feedUrl={`https://api.allorigins.win/get?url=${encodeURIComponent(data.feedUrl)}`}
           lowResCover={data.lowResCover}
           title={data.title}
           artist={data.artist}
-          description={data.description}
         />
       </section>
 
