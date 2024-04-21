629e137d29b2d37da16a9a33f2ea50ae0854a99f
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-21 21:03:46 +0200

    Increase Podcaster title size and added placeholder pending component for the navbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,21 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
-  component: () => (
+  pendingComponent: LoadingComponent,
+  component: RootComponent,
+});
+
+function RootComponent() {
+
+  return (
     <>
       <div className="p-2 flex gap-2">
-        <nav className='container mx-auto'>
-          <Link to="/" className="font-medium text-lg text-sky-600 hover:underline">
+        <nav className="container mx-auto">
+          <Link
+            to="/"
+            className="font-medium text-2xl text-sky-600 hover:underline"
+          >
             Podcaster
           </Link>
         </nav>
@@ -15,5 +24,26 @@ export const Route = createRootRoute({
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
     </>
-  ),
-})
+  );
+}
+
+function LoadingComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <nav className="container mx-auto">
+          <Link
+            to="/"
+            className="font-medium text-lg text-sky-600 hover:underline"
+          >
+            LOADING
+          </Link>
+          <p>Loading...</p>
+        </nav>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
