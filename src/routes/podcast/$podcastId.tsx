1f5f0a3965f0e9d17e85b5e0d038a9ac694381d8
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-22 01:01:28 +0200

    Add feedUrl as a prop to PodcastDetailsCard and added bottom padding

--- src/routes/podcast/$podcastId.tsx
+++ src/routes/podcast/$podcastId.tsx
@@ -7,8 +7,7 @@ import EpisodesTable from "../../components/episodes-table";
 export const Route = createFileRoute("/podcast/$podcastId")({
   loader: async ({ params }) => {
     const podcastDetails = await fetchPodcastDetails(params.podcastId);
-    if (!podcastDetails)
-      throw new Error("Something went wrong with the podcast details.");
+    if (!podcastDetails) throw new Error("Something went wrong with the podcast details.");
     return podcastDetails;
   },
   onError: ({ error }) => console.error(error),
@@ -24,13 +23,13 @@ function PodcastPage() {
   const data = Route.useLoaderData();
 
   return (
-    <main className="pt-8 px-4 container mx-auto max-lg:space-y-8 lg:flex gap-x-4 xl:gap-x-12">
+    <main className="py-8 px-4 container mx-auto max-lg:space-y-8 lg:flex gap-x-4 xl:gap-x-12">
       <section className="flex justify-center lg:block w-full">
         <PodcastDetailsCard
+          feedUrl={`https://api.allorigins.win/get?url=${encodeURIComponent(data.feedUrl)}`}
           lowResCover={data.lowResCover}
           title={data.title}
           artist={data.artist}
-          description={data.description}
         />
       </section>
 
