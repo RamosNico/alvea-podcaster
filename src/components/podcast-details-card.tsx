41c2ef38d3ad844ed1a5126d6215fc7b766623a2
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2024-04-22 02:10:21 +0200

    Add a spinner when loading description in podcast-details-card

--- src/components/podcast-details-card.tsx
+++ src/components/podcast-details-card.tsx
@@ -1,6 +1,7 @@
 import fetchXML from "../utils/fetch-xml";
 import Card from "./card";
-import { useQuery}  from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "./spinner";
 
 interface Props {
   feedUrl: string;
@@ -10,18 +11,21 @@ interface Props {
 }
 
 const PodcastDetailsCard = ({ feedUrl, lowResCover, title, artist }: Props) => {
-  const { data } = useQuery({queryKey: [feedUrl], queryFn: async () => {
-    try {
-      const feedData: any = await fetchXML(feedUrl)
+  const { data } = useQuery({
+    queryKey: [feedUrl],
+    queryFn: async () => {
+      try {
+        const feedData: any = await fetchXML(feedUrl);
 
-      return {
-        podcastCover: feedData.rss.channel.image.url,
-        podcastDescription: feedData.rss.channel.description,
-      };
-    } catch (error: any) {
-      console.error("Something went wrong while fetching the feedUrl.")
-    }
-  }})
+        return {
+          podcastCover: feedData.rss.channel.image.url,
+          podcastDescription: feedData.rss.channel.description,
+        };
+      } catch (error: any) {
+        console.error("Something went wrong while fetching the feedUrl.");
+      }
+    },
+  });
 
   return (
     <Card className="w-80 lg:w-72">
@@ -41,7 +45,14 @@ const PodcastDetailsCard = ({ feedUrl, lowResCover, title, artist }: Props) => {
 
         <section className="p-4">
           <p className="font-bold">Description</p>
-          <p className="break-words">{data?.podcastDescription || "Loading description..."}</p>
+          {data?.podcastDescription && (
+            <p className="break-words">{data.podcastDescription}</p>
+          )}
+          {!data?.podcastDescription && (
+            <div className="flex items-center gap-x-2">
+              <p>Loading description</p> <Spinner className="w-5 h-5" />
+            </div>
+          )}
         </section>
       </div>
     </Card>
